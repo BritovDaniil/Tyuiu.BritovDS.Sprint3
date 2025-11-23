701b45f8c259a0641d74c2367e52cefee82aa469
user <user@WIN-FJ5S0KDD7IV>
2025-11-23 21:26:43 +0300

    Task3

---
+++ Tyuiu.BritovDS.Sprint3.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BritovDS.Sprint3.Task3.V28.Lib;
+
+
+namespace Tyuiu.BritovDS.Sprint3.Task3.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            char item = 'r';
+
+            string value = "f35hyt t4j 3gkg45";
+
+            Assert.AreEqual("frrhyt trj rgkgrr", ds.ReplaceNumOnChar(value, item));
+        }
+    }
+}
