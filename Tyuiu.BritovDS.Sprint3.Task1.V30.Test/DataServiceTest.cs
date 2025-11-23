7d63603e8355b0c1523f4b3d379fd9575f3476f8
user <user@WIN-FJ5S0KDD7IV>
2025-11-23 18:48:36 +0300

    Task1

---
+++ Tyuiu.BritovDS.Sprint3.Task1.V30.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BritovDS.Sprint3.Task1.V30.Lib;
+namespace Tyuiu.BritovDS.Sprint3.Task1.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValidSum()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 12;
+            double value = 0.5;
+            double wait = 3.589;
+            var res = ds.GetSumSeries(value, startValue, stopValue);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
